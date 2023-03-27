196f37c0dd0be7eec8f3ed165fcf30ddd322f84d
MariaSRomero <s8romeromariadolores781@gmail.com>
2023-03-27 14:02:32 -0600

    modificar campo usuario y contraseña

--- lib/UI/srccens/inputs_screens.dart
+++ lib/UI/srccens/inputs_screens.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class InputsScreens extends StatefulWidget {
@@ -20,20 +22,71 @@ class _InputsScreensState extends State<InputsScreens> {
         horizontal: 10.0,
         vertical: 20.0),
         children: [
-          _crearEntrada()
+          _crearEntradaNombre(),
+          _crearEntradaPassword()
         ],
       ),
     );
   }
 
-  Widget _crearEntrada(){
+  Widget _crearEntradaNombre(){
     return TextField(
       autofocus: true,
-      textCapitalization: TextCapitalization.words,
+      textAlign: TextAlign.center,
+      textCapitalization: TextCapitalization.sentences,
+      style: const TextStyle(
+        color: Color.fromARGB(255, 23, 98, 238),
+        fontWeight: FontWeight.bold,
+      ) ,
+      cursorColor: const Color.fromARGB(255, 22, 72, 129),
+      cursorRadius: const Radius.circular(16.0),
+      cursorWidth: 3.0,
+      //maxLength: 4,
+      maxLines: null,
       decoration: InputDecoration(
         border: OutlineInputBorder(
           borderRadius:BorderRadius.circular(20.0)),
-      ),
+          hintText: "Escriba su nombre",
+          labelText: "Nombre:",
+          //helperText: "Nombre",
+          //Al ocupar el prefixIcon, el iconColor queda sin función
+          prefixIcon: const Icon(Icons.person_rounded),
+          //iconColor solo aplica para para la propiedad icon
+          //iconColor: const Color.fromARGB(255, 40, 183, 255),
+          // prefix: const CircularProgressIndicator(),
+          ),
+      onChanged: (valor){
+        _userName = valor;
+        print(_userName);
+      },
+    );
+  }
+
+  Widget _crearEntradaPassword(){
+    return TextField(
+      autofocus: true,
+      textAlign: TextAlign.center,
+      textCapitalization: TextCapitalization.sentences,
+      style: const TextStyle(
+        color: Color.fromARGB(255, 23, 98, 238),
+        fontWeight: FontWeight.bold,
+      ) ,
+      cursorColor: const Color.fromARGB(255, 22, 72, 129),
+      cursorRadius: const Radius.circular(16.0),
+      cursorWidth: 3.0,
+      maxLength: 8,
+      obscureText: true,
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius:BorderRadius.circular(20.0)),
+          hintText: "Escriba su contraseña",
+          labelText: "Contraseña:",
+          suffixIcon: const Icon(Icons.key),
+          //iconColor solo aplica para para la propiedad icon
+          //iconColor: const Color.fromARGB(255, 40, 183, 255),
+          // prefix: const CircularProgressIndicator(),
+          ),
       onChanged: (valor){
         _userName = valor;
         print(_userName);
